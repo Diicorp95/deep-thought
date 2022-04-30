84f57e2606e1232a040217ae1af4636eaadb17ec
Larry Holst <larry.holst@disroot.org>
2022-04-30 21:13:04 +0300

    Black + manual formatting

--- deep-thought.py
+++ deep-thought.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
-'''
+"""
     Program Name: Deep Thought (artifical intellegence)
-    Version     : 0.5.2
+    Version     : 0.5.2.1
     Author      : Diicorp95
     License     : UNLICENSE license
-'''
+"""
 
 import random       # Random data
 import json         # Storing training_data
@@ -17,7 +17,7 @@ import itertools    # Cleaning input - Making a chain
 
 # Constants
 TEXTS = [
-    '''
+    """
       ____
      / __ \\ ___   ___    ____
     / / / // _ \\ / _ \\  / __ \\
@@ -31,22 +31,22 @@ TEXTS = [
                                 /____/
 
  > Deep Thought - artifical intellegence
- - Version 0.5.2
+ - Version 0.5.2.1
  > Code was written by Larry Holst (Diicorp95)
  - UNLICENSE license
-    ''',
-    '''
+    """,
+    """
  * Training data loaded from {0:s}
  - {1:d}/{2:d} entries
  - {3:.3g} KB
-    ''',
-    '''
+    """,
+    """
  * File {0:s} is missing
-    ''',
-    '''
+    """,
+    """
  * Cleared training data
-    ''',
-    '''
+    """,
+    """
  > Help
  # Arguments
    Options: [ [-h | --help | /h|  /?] | [-t|  --training| /t] [filename] ]
@@ -88,56 +88,65 @@ TEXTS = [
  # Usage
    On the start, the script waits for user text input. The input is a question
    that the Deep Thought is being asked about.
-'''
+""",
 ]
 FILE_EXTENSION = ".trn"
 CLEAN_TRAINING = "/dev/null"
-POSSIBLE_A1 = {
-    '--help': 4, '-h': 4, '/?': 4, '/h': 4,
-    '--training': 10, '-t': 10, '/t': 10,
+POSSIBLE_ARGS = {
+    "--help": 4,
+    "-h": 4,
+    "/?": 4,
+    "/h": 4,
+    "--training": 10,
+    "-t": 10,
+    "/t": 10,
 }
 
 # Classes
 class Training:
     def __init__(self, d_list):
         self.d_list = d_list
+
     def update(self, d_list):
         self.d_list = d_list
+
 
 # Functions
 def text_print(index, *format_data):
     form = TEXTS[index]
     if format_data:
-        #print(list(format_data))
         form = form.format(*format_data)
     print(form)
 
+
 def clean_string(dirty):
     achars = (chr(i) for i in range(sys.maxunicode))
-    cats = {'Cc'}
+    cats = {"Cc"}
     _ = [c for c in achars if unicodedata.category(c) in cats]
-    ctrl_chars = ''.join(_)
-    _ = map(chr, itertools.chain(range(0x00,0x20), range(0x7f,0xa0)))
-    ctrl_chars = ''.join(_)
-    ctrl_char_re = re.compile('[%s]' % re.escape(ctrl_chars))
-    return ctrl_char_re.sub('', dirty)
+    ctrl_chars = "".join(_)
+    _ = map(chr, itertools.chain(range(0x00, 0x20), range(0x7F, 0xA0)))
+    ctrl_chars = "".join(_)
+    ctrl_char_re = re.compile("[%s]" % re.escape(ctrl_chars))
+    return ctrl_char_re.sub("", dirty)
+
 
 def process_path_info(filepath = False):
     if isinstance(filepath, bool) and filepath == False:
-        return os.path.splitext(sys.argv[0])[0]+FILE_EXTENSION
+        return os.path.splitext(sys.argv[0])[0] + FILE_EXTENSION
     elif isinstance(filepath, str):
         try:
             if not os.path.splitext(filepath)[1] == FILE_EXTENSION:
-                _ = os.path.splitext(sys.argv[0])[0]+FILE_EXTENSION
+                res = os.path.splitext(sys.argv[0])[0] + FILE_EXTENSION
             else:
-                _ = filepath
+                res = filepath
         except IndexError:
-            _ = os.path.splitext(sys.argv[0])[0]+FILE_EXTENSION
-        return _
-    return ''
+            res = os.path.splitext(sys.argv[0])[0] + FILE_EXTENSION
+        return res
+    return ""
+
 
 def load_json():
-    filepath = ''
+    filepath = ""
     empty_list = None
     try:
         if (sys.argv[2])[0:9] == CLEAN_TRAINING:
@@ -161,9 +170,10 @@ def load_json():
             entries = len(list(train.d_list.keys()))
             r_entries = len(list(x.keys()))
             kibis = round(os.path.getsize(filepath) / 1024, 3)
-            return [0,basename,entries,r_entries,kibis]
-        return [5,basename]
+            return [0, basename, entries, r_entries, kibis]
+        return [5, basename]
     return [100]
+
 
 def args_work():
     try:
@@ -181,7 +191,7 @@ def args_work():
         if argc > 0:
             _ = sys.argv[1]
             try:
-                x = POSSIBLE_A1.get(_.lower())
+                x = POSSIBLE_ARGS.get(_.lower())
             except IndexError:
                 return 0
             if x > -1:
@@ -193,27 +203,23 @@ def args_work():
         return 1
     except UnicodeDecodeError:
         try:
-            # _ = ''
-            # try:
-            #     _ = os.path.basename(sys.argv[2])
-            # except IndexError:
-            #     _ = os.path.splitext(sys.argv[0])[0]+FILE_EXTENSION
             _ = process_path_info()
-            return [4,_]
+            return [4, _]
         except IndexError:
-            return [4,None]
+            return [4, None]
     except IndexError:
         return 0
 
+
 def set_seed_from_user(the_input):
-    if isinstance(the_input, str) and not the_input == "":
+    if isinstance(the_input, str) and the_input:
         proc_input = 0
         for i in the_input:
             proc_input = (proc_input << 1) + (ord(i) % 16)
         nseed = proc_input
         if len(the_input) > proc_input:
-            for i in range(0,len(the_input)):
-                j = proc_input+i
+            for i in range(0, len(the_input)):
+                j = proc_input + i
                 try:
                     nseed &= chr(the_input[j]) % 1
                 except TypeError:
@@ -222,53 +228,57 @@ def set_seed_from_user(the_input):
         return 0
     return 1
 
+
 def nth_keyname(dic, n):
     try:
         _ = list(dic.keys())[n]
-        if isinstance(_,str):
+        if isinstance(_, str):
             return _
         return False
     except IndexError:
         return False
 
+
 def random_bit_array(length):
     the_array = []
     x = [1] * 2
-    for i in range(0,length):
+    for i in range(0, length):
         the_array.append(int(bin(x[0] * x[1])[2:]))
         x[1] += random.getrandbits(1)
         x[0] = ((x[0] << 2) & (x[0] + 1)) | random.getrandbits(1)
     return the_array
 
+
 def parse_raw(info):
-    the_string = ''
+    the_string = ""
     switch = train.d_list
-    for i,_ in enumerate(switch):
-        _ = nth_keyname(switch,i)
+    for i, key in enumerate(switch):
+        key = nth_keyname(switch, i)
         if not _ is False:
             if info.find(_) > -1:
                 return switch[_]
     return info
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     train = Training({})
 
-    for i,j in enumerate(TEXTS):
+    for i, j in enumerate(TEXTS):
         TEXTS[i] = j.rstrip()
     text_print(0)
 
     _ = args_work()
     if type(_) == list:
         if _[0] == 0:
-            text_print(1,_[1],_[2],_[3],_[4])
+            text_print(1, _[1], _[2], _[3], _[4])
         elif _[0] == 5:
             try:
                 if not _[2] == True:
                     raise BaseException
             except BaseException:
-                text_print(2,_[1])
+                text_print(2, _[1])
         elif _[0] == 6:
-            text_print(4,_[1])
+            text_print(4, _[1])
         elif _[0] == 100:
             text_print(3)
     else:
@@ -277,34 +287,34 @@ if __name__=="__main__":
             text_print(cor_text)
             raise SystemExit
 
-    while (True):
-        while (True):
+    while True:
+        while True:
             try:
                 user_i = input("\nUser - ")
             except KeyboardInterrupt:
                 raise SystemExit
-            if user_i == "":
+            if not user_i:
                 raise SystemExit
             break
         user_i = user_i.strip().upper()
         set_seed_from_user(user_i)
-        print('Computer - Hmm...')
+        print("Computer - Hmm...")
 
-        accumulator = ''
-        for i,j in enumerate(random_bit_array(2**8)):
+        accumulator = ""
+        for i, j in enumerate(random_bit_array(2 ** 8)):
             accumulator += str(j)
-            accumulator = ''.join(random.sample(accumulator,len(accumulator)))
+            accumulator = "".join(random.sample(accumulator, len(accumulator)))
 
-        for n in range(0,2**3):
-            thought = ''
-            for i in range(0,len(accumulator) % 16):
-                thought += chr(int(str(accumulator)[i:i+8],2))
+        for n in range(0, 2 ** 3):
+            thought = ""
+            for i in range(0, len(accumulator) % 16):
+                thought += chr(int(str(accumulator)[i : i + 8], 2))
             output = clean_string(thought.strip())
-            if output == "":
+            if not output:
                 output = "Nothing"
             else:
                 output = parse_raw(str(output))
             if not output == "Nothing":
                 break
 
-        print('Computer -',output)
+        print("Computer -", output)
